d223e9a0c89a1324aeb4eae75f3fd0596b231f86
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-10-11 21:11:34 +0200

    Install drizzle

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div className="w-screen min-h-screen bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        {children}
+      </div>
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,46 @@
 import { Button } from "@/components/ui/button"
+import { UserButton, auth } from "@clerk/nextjs"
+import { ArrowRight, LogIn } from "lucide-react"
+import Link from "next/link"
 
-export default function Home() {
-  return <Button className="text-red-600">Hello</Button>
+export default async function Home() {
+  const { userId } = await auth()
+  const isAuth = !!userId
+  return (
+    <div className="w-screen min-h-screen bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-col items-center text-center">
+          <div className="flex items-center">
+            {" "}
+            <h1 className="mr-3 text-5xl font-semibold">Chat with any PDF</h1>
+            <UserButton afterSignOutUrl="/" />
+          </div>
+          <div className="flex mt-2">
+            {isAuth && (
+              <>
+                <Button>
+                  Go to Chats <ArrowRight className="ml-2" />
+                </Button>
+              </>
+            )}
+          </div>
+          <p className="max-w-xl mt-1 text-lg text-slate-600">
+            Join millions of students, researchers and professionals to
+            instantly answer questions and understand research with AI
+          </p>
+          <div className="w-full mt-4">
+            {isAuth ? (
+              <h1>fileUpload</h1>
+            ) : (
+              <Link href={"/sign-in"}>
+                <Button>
+                  Login to get Started <LogIn className="w-4 h-4 ml-2" />
+                </Button>
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
